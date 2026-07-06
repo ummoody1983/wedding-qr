146a9343f55b713d14eeb764dde6a36b289d7683
ummoody1983 <ummoody.1983@gmail.com>
2026-07-06 15:03:41 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // رابط Google Apps Script الخاص بالنظام
-const API_URL = "https://script.google.com/macros/s/AKfycbzupJg5Gb9U84GmqurOyMvfWwqL59DrLW_mPCJRomY-ykRcfYVECwIOjjcEaZbVGSmTVA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxgDgvjCjgpdp0smKoz1eQr2k05z_ZmW9BRhAFE3d1fBfO2fEofHPTn-8ByoeLm7Tcd_A/exec";
